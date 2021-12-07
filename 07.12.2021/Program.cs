920e0c1e76680f80418e42e1cf802cc4f61c4e7a
MOKProgramist <is24romanovar@artcollege.ru>
2021-12-07 11:09:21 +0300

    Добавьте файлы проекта.

---
+++ 07.12.2021/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._12._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mas = new double[5];
+            
+            for(int i = 0; i < 5; i++)
+            {
+                // запись ввода в массив
+                Console.Write($"Введите {i} элемент массива: ");
+                mas[i] = double.Parse(Console.ReadLine());
+            }
+
+            // вывод элементов массива
+            Console.WriteLine();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine($"mas[{i}] = {mas[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
